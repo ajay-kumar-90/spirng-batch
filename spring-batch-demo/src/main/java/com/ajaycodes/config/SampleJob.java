81236af40a7f877864ec6bb5b0208c4c127f59b2
Ajay Kumar <ajay_kumar_1990@live.com>
2023-03-19 23:03:27 +0530

    Spring batch 5 upgrade

--- spring-batch-demo/src/main/java/com/ajaycodes/config/SampleJob.java
+++ spring-batch-demo/src/main/java/com/ajaycodes/config/SampleJob.java
@@ -7,53 +7,56 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.ajaycodes.service.SecondTasklet;
 
 @Configuration
 public class SampleJob {
-	
-	
+
 	private static final Logger log = LoggerFactory.getLogger(SampleJob.class);
 
-	
-	//JobBuilderFactory to create Job
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
-	
-	//StepBuilerFactory to create Step
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
-	
-	@Autowired
 	private SecondTasklet secondTasklet;
+	
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    
+	SampleJob(SecondTasklet secondTasklet, JobRepository jobRepository,
+			PlatformTransactionManager transactionManager) {
+		super();
+		this.secondTasklet = secondTasklet;
+		this.jobRepository = jobRepository;
+		this.transactionManager = transactionManager;
+	}
 
-    // define bean of type job
-    @Bean
-    Job firstJob() {
-		return jobBuilderFactory.get("First Job")
-		// for first step
-		.start(firstStep())
-		// for next all steps
-		.next(secondStep())
-		.build();
+	// define bean of type job
+	@Bean
+	Job firstJob() {
+		return new JobBuilder("First Job", jobRepository)
+				// for first step
+				.start(firstStep())
+				// for next all steps
+				.next(secondStep()).build();
 	}
 
 	// define a step
-	private Step firstStep() {
-		return stepBuilderFactory.get("First Step")
-		.tasklet(firstTask())
-		.build();
+	@Bean
+	Step firstStep() {
+		return new StepBuilder("First Step", jobRepository)
+				.tasklet(firstTask(), transactionManager)
+				.build();
 	}
-	
+
 	// define a tasklet
 	private Tasklet firstTask() {
 		// task inside the step
@@ -65,23 +68,13 @@ public class SampleJob {
 			}
 		};
 	}
-	
+
 	// define second step
-		private Step secondStep() {
-			return stepBuilderFactory.get("Second Step")
-			.tasklet(secondTasklet)
+	@Bean
+	Step secondStep() {
+	return new StepBuilder("Second Step", jobRepository)
+			.tasklet(secondTasklet, transactionManager)
 			.build();
-		}
-		
-//		// define second tasklet
-//		private Tasklet secondTask() {
-//			// task inside the step
-//			return new Tasklet() {
-//				@Override
-//				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-//					System.out.println("This is second tasklet step");
-//					return FINISHED;
-//				}
-//			};
-//		}
+	}
+
 }
